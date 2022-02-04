2a6c62f84ce81adb69cb88ed9f9e4c982ac7684b
Uanderson Santana <udsantos@indracompany.com>
2022-02-04 07:46:35 -0300

    161. Implementando movimentos possiveis da torre.

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -29,6 +29,11 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
+	
+	public void setValues(int row, int colomn) {
+		this.row = row;
+		this.column = column;
+	}
 
 
 	@Override
